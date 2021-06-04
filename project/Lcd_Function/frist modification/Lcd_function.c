96ec5ec76527b3503eb641ab1153fe3c1093ab19
MohamedElgarhy9 <1902228@eng.asu.edu.eg>
2021-06-04 18:55:53 +0200

    Createed the main structure of the projsct

---
+++ project/Lcd_Function/frist modification/Lcd_function.c
@@ -0,0 +1,58 @@
+#include "tm4c123gh6pm.h"
+#include <stdint.h>
+void SystemInit(){}
+
+
+
+
+
+void LCD_Command(char com){
+    GPIO_PORTA_DATA_R=0;
+    GPIO_PORTB_DATA_R=com;
+    GPIO_PORTA_DATA_R |=0x80;
+    int x;
+    x=x+100;
+    GPIO_PORTA_DATA_R=0;
+
+
+    }
+void LCD_DATA(char data){
+    GPIO_PORTA_DATA_R=0x20;
+    GPIO_PORTB_DATA_R=data;
+    GPIO_PORTA_DATA_R |=0x80;
+    int x;
+    x=x+100;
+    GPIO_PORTA_DATA_R=0;
+
+
+    }
+
+
+void Init(void){
+		SYSCTL_RCGCGPIO_R |= 0x03;
+		GPIO_PORTB_DIR_R=0xFF;
+		GPIO_PORTB_DEN_R=0xFF;
+		GPIO_PORTA_DIR_R=0xE0;
+		GPIO_PORTA_DEN_R=0xE0;
+		GPIO_PORTB_AFSEL_R=0;
+		GPIO_PORTB_AMSEL_R=0;
+		GPIO_PORTB_PCTL_R=0;
+		LCD_Command(0x01);
+		LCD_Command(0x02);
+		LCD_Command(0x06);
+		LCD_Command(0x0F);
+		LCD_Command(0x30);
+		LCD_Command(0x38);
+
+    }
+
+
+
+
+int main(){
+	Init();
+		while(1){
+		LCD_DATA(10);
+
+		}
+}
