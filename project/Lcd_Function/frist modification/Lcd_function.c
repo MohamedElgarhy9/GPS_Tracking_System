30f3dfebfc05ac890dfbfd2983681a730ee4184a
MohamedElgarhy9 <78216592+MohamedElgarhy9@users.noreply.github.com>
2021-06-05 18:38:41 +0200

    Adding lock & commit registers for portA and portB
    Although adding  SYSCTL_PRGPIO_R to check the clock enable

--- project/Lcd_Function/frist modification/Lcd_function.c
+++ project/Lcd_Function/frist modification/Lcd_function.c
@@ -30,21 +30,26 @@ void LCD_DATA(char data){
     }
 
 
-void Init(void){
-		SYSCTL_RCGCGPIO_R |= 0x03;
-		GPIO_PORTB_DIR_R=0xFF;
-		GPIO_PORTB_DEN_R=0xFF;
-		GPIO_PORTA_DIR_R=0xE0;
-		GPIO_PORTA_DEN_R=0xE0;
-		GPIO_PORTB_AFSEL_R=0;
-		GPIO_PORTB_AMSEL_R=0;
-		GPIO_PORTB_PCTL_R=0;
-		LCD_Command(0x01);
-		LCD_Command(0x02);
-		LCD_Command(0x06);
-		LCD_Command(0x0F);
-		LCD_Command(0x30);
-		LCD_Command(0x38);
+void Init(void){       
+	SYSCTL_RCGCGPIO_R |= 0x03;
+        while ((SYSCTL_PRGPIO_R&0x03) == 0){};
+        GPIO_PORTA_LOCK_R = 0x4C4F434B;
+        GPIO_PORTB_LOCK_R = 0x4C4F434B;
+        GPIO_PORTB_CR_R |= 0xFF;
+        GPIO_PORTA_CR_R |= 0xE0;
+        GPIO_PORTB_DIR_R|=0xFF;
+        GPIO_PORTB_DEN_R|=0xFF;
+        GPIO_PORTA_DIR_R|=0xE0;
+        GPIO_PORTA_DEN_R|=0xE0;
+        GPIO_PORTB_AFSEL_R=0;
+        GPIO_PORTB_AMSEL_R=0;
+        GPIO_PORTB_PCTL_R=0;
+        LCD_Command(0x01);
+        LCD_Command(0x02);
+        LCD_Command(0x06);
+        LCD_Command(0x0F);
+        LCD_Command(0x30);
+        LCD_Command(0x38);
 
     }
 
