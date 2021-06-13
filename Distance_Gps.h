d9e84d24899441276602cda471bc1857518f6478
ahmedsayed123 <85370880+ahmedsayed123@users.noreply.github.com>
2021-06-13 23:38:49 +0200

    Delete Distance_Gps.h

--- Distance_Gps.h
+++
@@ -1,9 +0,0 @@
-#ifndef DISTANCE_H_INCLUDED
-#define DISTANCE_H_INCLUDED
-#include "math.h"
-#include "stdlib.h"
-#include "stdint.h"
-
-long double dist(long double th1,long double ph1,long double th2,long double ph2);
-
-#endif
