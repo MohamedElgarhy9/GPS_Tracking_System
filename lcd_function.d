a4015c5d74f7ddd566103d238046ed1e396bf717
Loay20 <85364998+Loay20@users.noreply.github.com>
2021-06-13 23:39:08 +0200

    Delete lcd_function.d

--- lcd_function.d
+++
@@ -1,5 +0,0 @@
-.\lcd_function.o: Lcd_Function.c
-.\lcd_function.o: Lcd_Function.h
-.\lcd_function.o: tm4c123gh6pm.h
-.\lcd_function.o: delay.h
-.\lcd_function.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
