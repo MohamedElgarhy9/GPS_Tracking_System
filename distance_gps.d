9d3e97821b8eedb255d7c442682b4186a37dc8ae
Loay20 <85364998+Loay20@users.noreply.github.com>
2021-06-13 23:39:26 +0200

    Delete distance_gps.d

--- distance_gps.d
+++
@@ -1,6 +0,0 @@
-.\distance_gps.o: Distance_Gps.c
-.\distance_gps.o: Distance_Gps.h
-.\distance_gps.o: C:\Keil\ARM\ARMCC\bin\..\include\math.h
-.\distance_gps.o: C:\Keil\ARM\ARMCC\bin\..\include\stdlib.h
-.\distance_gps.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
-.\distance_gps.o: C:\Keil\ARM\ARMCC\bin\..\include\stdio.h
