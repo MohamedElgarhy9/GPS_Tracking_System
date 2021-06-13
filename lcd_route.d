359eb1d545da420eaa56562e78b80717ebbfb4c3
Loay20 <85364998+Loay20@users.noreply.github.com>
2021-06-13 23:38:59 +0200

    Delete lcd_route.d

--- lcd_route.d
+++
@@ -1,6 +0,0 @@
-.\lcd_route.o: Lcd_route.c
-.\lcd_route.o: lcd_route.h
-.\lcd_route.o: tm4c123gh6pm.h
-.\lcd_route.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
-.\lcd_route.o: Lcd_Function.h
-.\lcd_route.o: delay.h
